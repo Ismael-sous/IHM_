4f148ad97715505f89afdceb22b524b1c7df52dd
Ismael-sous <ismael.sousane@gmail.com>
2021-04-20 16:55:54 +0200

    Supp et modif

--- Main.java
+++ Main.java
@@ -1,38 +1,50 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 /**
- * Write a description of class FirstAttempt here.
+ * The StepTwo of JavafX tutorial
+ * Linking FXML and java code via java
  *
- * @author Patrick GIRARD
+ * @author Patrick Girard
  * @version 22/03/2020
  */
-
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-    
-    // Point d'entrée du programme
-    public static void main(String[] args) {
-        launch(); // Appel du point d'entrée de l'application JavaFx (voir ci-dessous)
-    }
-    
-    // Point d'entrée de l'application JavaFX
+public class Main extends Application
+{
+ 
+    /**
+     * The start method is the main entry point for every JavaFX application. 
+     * It is called after the init() method has returned and after 
+     * the system is ready for the application to begin running.
+     *
+     * @param  stage the primary stage for this application.
+     */
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception
+    {
+        // Building the FXML hierarchy
+        Parent root = FXMLLoader.load(getClass().getResource("accueil.fxml"));
+        
+        // JavaFX must have a Scene (window content) inside a Stage (window)       
+        Scene scene = new Scene (root);
+        
+        stage.setTitle("FXML Counter V1");
+        stage.getIcons().add(new Image("file:ressources/img/world-map.png"));
 
-        // Définition du titre de la fenêtre de l'application
-        primaryStage.setTitle("Titre de la fenêtre");
-        
-        // Appel d'une méthode permettant une fermeture propre de l'application
-        primaryStage.setOnCloseRequest(e -> Platform.exit());
-        
-        // Création de la scène (avec des dimensions correspondant à une résolution HD), composé uniquement d'une étiquette affichant du texte
-        primaryStage.setScene(new Scene(new Label("Hello wold !"), 1280, 720));
-        
-        // Affichage de la fenêtre
-        primaryStage.show();
+        stage.setScene(scene);
+        // Show the Stage (window)
+        stage.show();
+
     }
+
 }
