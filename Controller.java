ee911f060b6a7c900b0e37d92ad018c831cc8c3f
Auxane <sansaeva590@gmail.com>
2021-04-21 17:36:16 +0200

    remplissage tableau

--- Controller.java
+++ Controller.java
@@ -29,27 +29,29 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable
 {
     private Main mainApp;
-    @FXML
-    private TableView tableauListeEtu;
+    @FXML private TableView myTable;
 
     @FXML
     private Button listButton;
 
-    @FXML
-
     public void list() throws IOException
     {
-        Parent part = FXMLLoader.load(getClass().getResource("listeEtu.fxml"));
+//        Parent part = FXMLLoader.load(getClass().getResource("listeEtu.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("listeEtu.fxml"));
+        Parent part = loader.load();
         Stage stage = new Stage();
         Scene scene = new Scene(part);
         stage.setScene(scene);
+        stage.setTitle("Liste Etudiant");
+        stage.show();
         TableColumn prenom = new TableColumn("Prénom");
         TableColumn nom = new TableColumn("Nom");
         TableColumn annee = new TableColumn("Année");
         TableColumn promo = new TableColumn("Promotion");
         TableColumn option = new TableColumn("Option");
 
-        tableauListeEtu.getColumns().addAll(prenom, nom, annee, promo,option);
+        TableView myTable = (TableView) loader.getNamespace().get("myTable");
+        myTable.getColumns().addAll(prenom, nom, annee, promo,option);
 
         final ObservableList<Etudiant> data = FXCollections.observableArrayList(
                 new Etudiant("Jacob","Smith",1999,"M1","Biotech"),
@@ -61,10 +63,7 @@ public class Controller implements Initializable
         promo.setCellValueFactory(new PropertyValueFactory<Etudiant,String>("promo"));
         option.setCellValueFactory(new PropertyValueFactory<Etudiant,String>("option"));
 
-        tableauListeEtu.setItems(data);
-
-        stage.setTitle("Liste Etudiant");
-        stage.show();
+        myTable.setItems(data);
 
     }
 
